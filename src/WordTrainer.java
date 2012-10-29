154c0b768245d99870ba2337020b306183e85aea
Jan E <jeebjp@gmail.com>
2012-10-29 15:32:56 +0200

    Add the project itself, and modify .gitignore

---
+++ src/WordTrainer.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jeekstro
+ * Date: 29.10.2012
+ * Time: 15:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordTrainer {
+}
