aee4fca0469f7e1f9341cb492656b316f7720d34
Jan E <jeebjp@gmail.com>
2012-11-06 14:07:22 +0200

    Add some actual code, file handling and return values!
    Man, I hate java for taking the System.exit() route.

--- src/WordTrainer.java
+++ src/WordTrainer.java
@@ -1,3 +1,8 @@
+import java.io.FileReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jeekstro
@@ -6,4 +11,28 @@
  * To change this template use File | Settings | File Templates.
  */
 public class WordTrainer {
+
+    public static FileReader openFile(String filename) throws IOException {
+        FileReader fr = new FileReader(filename);
+        return fr;
+    }
+
+    /* Main function for testing purposes */
+    public static void main(String[] args) {
+        /* Check for argument count */
+        if(args.length < 1) {
+            System.out.println("Give at least one argument");
+        }
+
+        System.out.println("File name set: " + args[0]);
+
+        try( FileReader fr = openFile(args[0]) ) {
+
+        } catch(IOException ioe) {
+            System.out.println("Error happened: " + ioe.getMessage());
+            System.exit(1);
+        }
+
+        System.exit(0);
+    }
 }
