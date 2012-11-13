e053ceed1262f21ac1a4da41ffdb5d9e16e7830d
Jan E <jeebjp@gmail.com>
2012-11-13 19:04:02 +0200

    Add actual tests

--- tests/WordPairTest.java
+++ tests/WordPairTest.java
@@ -1,3 +1,6 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jeekstro
@@ -6,4 +9,37 @@
  * To change this template use File | Settings | File Templates.
  */
 public class WordPairTest {
+    @Test
+    public void testWordPairCreationThreeArgs() {
+        /* Predefined strings */
+        String herp    = "herp";
+        String derp    = "derp";
+        String comment = "comment";
+
+        /* Create new wordpair */
+        WordPair wp = new WordPair(herp, derp, comment);
+
+        /* Assert that the thing is not null */
+        Assert.assertNotNull(wp);
+
+        /* Assert that the things we get from it equal what we supposedly set */
+        Assert.assertTrue(wp.getWord().equals(herp) && wp.getPair().equals(derp) &&
+                          wp.getComment().equals(comment));
+    }
+
+    @Test
+    public void testWordPairCreationTwoArgs() {
+        /* Predefined strings */
+        String herp = "herp";
+        String derp = "derp";
+
+        /* Create new wordpair */
+        WordPair wp = new WordPair(herp, derp);
+
+        /* Assert that the thing is not null */
+        Assert.assertNotNull(wp);
+
+        /* Assert that the things we get from it equal what we supposedly set */
+        Assert.assertTrue(wp.getWord().equals(herp) && wp.getPair().equals(derp));
+    }
 }
