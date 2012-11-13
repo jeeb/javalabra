d528a1d7fb512db08156ece96045d80985a1cfe2
Jan E <jeebjp@gmail.com>
2012-11-13 18:44:56 +0200

    Move file parsing into its own function

--- src/WordTrainer.java
+++ src/WordTrainer.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.lang.Integer;
-import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -17,38 +16,24 @@ import java.util.Scanner;
  */
 public class WordTrainer {
 
-    /**
-     * Main function (for testing currently)
-     * @param args command line arguments given
-     */
-    public static void main(String[] args) {
+    public static WordPairContainer parseWordPairFile(Path file_path) {
         /* Set a semi-sane character set for input/output */
         Charset charset = Charset.forName("UTF-8");
 
-        String line = null;
-        WordPairContainer wpc = new WordPairContainer();
-
-        /* Check for argument count */
-        if( args.length < 1 ) {
-            System.err.println("Error: Give at least one argument");
-            System.exit(1);
-        }
-
-        /* Print the file name used for debug purposes, and create the path */
-        System.err.println("File name set: " + args[0]);
-        Path file_path = Paths.get(args[0]);
+        String            line = null;
+        WordPairContainer wpc  = new WordPairContainer();
 
         /* Try opening the file */
         try( BufferedReader reader = Files.newBufferedReader(file_path, charset) ) {
             /* Try reading the first line of the file */
             if( (line = reader.readLine()) == null ) {
                 System.err.println("Error: EOF before magic");
-                System.exit(1);
+                return null;
             }
 
             if( !line.equals("WORDFILE") ) {
                 System.err.println("Error: Word file magic not found");
-                System.exit(1);
+                return null;
             }
 
             /* If we got here, we have the WORDFILE magic, and we could read the file */
@@ -56,7 +41,7 @@ public class WordTrainer {
             /* Try reading the second line of the file */
             if( (line = reader.readLine()) == null ) {
                 System.err.println("Error: EOF before file spec version");
-                System.exit(1);
+                return null;
             }
 
             /* Try parsing an integer out of the read line */
@@ -65,7 +50,7 @@ public class WordTrainer {
             /* Current file structure version is 1 */
             if( file_version != 1 ) {
                 System.err.println("Error: File structure version is incorrect");
-                System.exit(1);
+                return null;
             }
 
             /* If we got here, we have the file header read */
@@ -80,13 +65,13 @@ public class WordTrainer {
 
                 if( cutparts.length < 2 ) {
                     System.err.println("Warning: File has less than two delimited parts on line " + curline + ". " +
-                                       "Ignoring.");
+                            "Ignoring.");
                     continue;
                 }
 
                 if( cutparts[0].equalsIgnoreCase("") || cutparts[1].equalsIgnoreCase("") ) {
                     System.err.println("Warning: File has an empty string before or after delimiter on line " +
-                                       curline + ". Ignoring.");
+                            curline + ". Ignoring.");
                     continue;
                 }
 
@@ -104,10 +89,40 @@ public class WordTrainer {
         } catch( IOException ioe ) {
             /* If le file opening failed, we print an error and exit */
             System.err.println("Error during file I/O:\n\t" + ioe);
-            System.exit(1);
+            return null;
         } catch( NumberFormatException nfe ) {
             /* If we have a fail with parsing an integer from the file header, we give an error and exit */
             System.err.println("Error during parsing an integer from a string:\n\t" + nfe);
+            return null;
+        }
+
+        return wpc;
+    }
+
+    /**
+     * Main function (for testing currently)
+     * @param args command line arguments given
+     */
+    public static void main(String[] args) {
+        /* Set a semi-sane character set for input/output */
+        Charset charset = Charset.forName("UTF-8");
+
+        WordPairContainer wpc  = null;
+
+        /* Check for argument count */
+        if( args.length < 1 ) {
+            System.err.println("Error: Give at least one argument");
+            System.exit(1);
+        }
+
+        /* Print the file name used for debug purposes, and create the path */
+        System.err.println("File name set: " + args[0]);
+
+        wpc = parseWordPairFile(Paths.get(args[0]));
+
+        /* Error state happened and null was returned */
+        if( wpc == null ) {
+            System.err.println("Error state happened, exiting program");
             System.exit(1);
         }
 
