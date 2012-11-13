b4e5c2633a0687bbceae701d65256c1c84734b08
Jan E <jeebjp@gmail.com>
2012-11-13 18:53:18 +0200

    Add WordPairTest class

---
+++ tests/WordPairTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jeekstro
+ * Date: 13.11.2012
+ * Time: 18:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordPairTest {
+}
