d9ae95b9627cc3b72cdb464939a16a784447e78e
Jan E <jeebjp@gmail.com>
2012-11-19 19:11:52 +0200

    WordPair: Add JavaDoc stubs

--- src/WordPair.java
+++ src/WordPair.java
@@ -10,26 +10,49 @@ public class WordPair {
     private String pair;
     private String comment;
 
+    /**
+     *
+     * @param word
+     * @param pair
+     * @param comment
+     */
     public WordPair(String word, String pair, String comment) {
         this.word    = word;
         this.pair    = pair;
         this.comment = comment;
     }
 
+    /**
+     *
+     * @param word
+     * @param pair
+     */
     public WordPair(String word, String pair) {
         this.word    = word;
         this.pair    = pair;
         this.comment = "";
     }
 
+    /**
+     *
+     * @return
+     */
     public String getWord() {
         return word;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPair() {
         return pair;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getComment() {
         return comment;
     }
