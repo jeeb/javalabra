d3153fc6b2daffa43b3053837d10ab72d22f717e
Jan E <jeebjp@gmail.com>
2012-11-19 16:16:02 +0200

    Make FileReader easier to use from the outside

--- src/FileReader.java
+++ src/FileReader.java
@@ -14,19 +14,15 @@ import java.nio.file.Paths;
  */
 public class FileReader {
     /**
-     * Takes in a string that is then interpreted as a Path, and then returns a
-     * BufferedReader object for further reading of the file.
+     * Takes in a Path object, and then returns a BufferedReader object for further reading of the file.
      *
      * Returns null if something failed.
-     * @param file_path_string a String that will be used as the path
+     * @param file_path a Path object that will be used for opening of the BufferedReader
      * @return A BufferedReader object if successful, otherwise returns null.
      */
-    public static BufferedReader openFile(String file_path_string) {
+    public static BufferedReader createBReader(Path file_path) {
         /* Set a semi-sane character set for reading the file */
         Charset charset = Charset.forName("UTF-8");
-
-        /* Try to read the passed string as a file path */
-        Path file_path = Paths.get(file_path_string);
 
         try {
             return Files.newBufferedReader(file_path, charset);
@@ -39,8 +35,9 @@ public class FileReader {
 
     /**
      * Takes in a BufferedReader object that is then used for reading the file and creating a WordPairContainer,
-     * that is then returned. If it fails, the function returns null.
+     * that is then returned.
      *
+     * Returns null if something fails, or if the file contains no entries.
      * @param reader a BufferedReader object that will be read in the function
      * @return A WordPairContainer object if successful and the file contains entries, otherwise returns null.
      * @throws IOException
@@ -112,6 +109,43 @@ public class FileReader {
         if( !wpc.isEmpty() ) {
             return wpc;
         } else {
+            return null;
+        }
+    }
+
+    /**
+     * Takes in a String that is then used to construct a
+     * @param file_path_string
+     * @return
+     */
+    public static WordPairContainer createWPCFromFile(String file_path_string) {
+        Path              file_path = null;
+        BufferedReader    br        = null;
+
+        /* Try to read the passed string as a file path */
+        file_path = Paths.get(file_path_string);
+
+        /* If the Path still points to null, we have an error state */
+        if( file_path == null ) {
+            System.err.println("Error state happened when creating the reader object!");
+            return null;
+        }
+
+        /* Try to create a BufferedReader from the Path */
+        br = createBReader(file_path);
+
+        /* If the BufferedReader still points to null, we have an error state */
+        if( br == null ) {
+            System.err.println("Error state happened when reading the file!");
+            return null;
+        }
+
+        /* Try parsing the file for which we got a BufferedReader into a WordPairContainer */
+        try {
+            return parseFileToWPC(br);
+        } catch (IOException ioe) {
+            /* If we got an exception, we have a fail */
+            System.err.println("I/O Error during file parsing:\n\t" + ioe);
             return null;
         }
     }
