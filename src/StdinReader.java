73cbbb3ce8aec647da0d7999352b5cfc3f0e6b69
Jan E <jeebjp@gmail.com>
2012-12-07 19:24:41 +0200

    src: Add StdinReader class

---
+++ src/StdinReader.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+/**
+ * Contains convenience functions for standard input reading
+ */
+public class StdinReader {
+    // We shall read in lolUTF-8
+    private static Charset charset = Charset.forName("UTF-8");
+
+    // We also have a BufferedReader here
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, charset));
+
+    public static String readString() {
+        String string = null;
+
+        try {
+            string = reader.readLine();
+            if( string == null ) {
+                return null;
+            }
+
+            return string;
+        } catch (IOException e) {
+            System.err.println(":: Error reading input:\n\t" + e);
+            return null;
+        }
+    }
+
+    public static Integer readInteger() {
+        String string = null;
+        int    i      = -1;
+
+        try {
+            string = reader.readLine();
+            if( string == null ) {
+                return null;
+            }
+
+            i = Integer.parseInt(string, 10);
+
+            return i;
+        } catch (IOException e) {
+            System.err.println(":: Error reading input:\n\t" + e);
+            return null;
+        } catch (NumberFormatException e) {
+            System.err.println(":: Error reading input:\n\t" + e);
+            return null;
+        }
+    }
+}
